7d8799db3fab3ea9158a7005237f9aadbe279e74
Alessander França <alessander70@gmail.com>
2015-07-05 19:57:12 -0300

    desktop done

--- js/footer.js
+++ js/footer.js
@@ -1,6 +1,7 @@
 window.onload = function(){
-    
+
     var footer = document.getElementById('footer');
+    var containerPage = document.getElementById('container-page');
     var footerArrow = document.getElementById('footer-arrow');
     var footerArrow_size = footerArrow.clientHeight;
     var footerC = document.getElementById('footer-container');
@@ -10,13 +11,12 @@ window.onload = function(){
     var twitterLogo = document.getElementById('twitter-logo');
     var linkedinLogo = document.getElementById('linkedin-logo');
     var open = false;
-    var window_height = window.innerHeight; 
-    var expanded_size = footerC_size + footerArrow_size;
+    var window_height = window.innerHeight;
 
     footer.onclick = function(){
         if(window.innerWidth < 768){
             if(open === false){
-                                                       
+
                 if(window.innerHeight < 550 && window.innerWidth >= 360 && window.innerWidth <= 480){
                     faceLogo.style.backgroundPositionX = '-80px';
                     twitterLogo.style.backgroundPositionY = '-80px';
@@ -25,11 +25,11 @@ window.onload = function(){
                     twitterLogo.style.opacity = '0';
                     linkedinLogo.style.opacity = '0';
                 }
-                openMenu();  
+                openMenu();
             }
             else {
-                
-                
+
+
                 if(window.innerHeight < 550 && window.innerWidth >= 360 && window.innerWidth <= 480){
                     faceLogo.style.backgroundPositionX = '0px';
                     twitterLogo.style.backgroundPositionY = '0px';
@@ -39,37 +39,37 @@ window.onload = function(){
                     linkedinLogo.style.opacity = '1';
                 }
                 closeMenu();
-                
+
             }
         }
-        
+
     }
-    
+
     window.onresize = function(){
-        
+
         if(window.innerWidth >= 768){
-            footer.style.height = 'auto';    
+            footer.style.height = 'auto';
         }
-        
+
         else{
             closeMenu();
-        }        
-        
+        }
+
     }
-    
+
     function openMenu(){
         footer.style.height = '171px';
-        footerArrow.classList.remove('arrow-up');                                                
-        footerArrow.classList.add('arrow-down');   
+        footerArrow.classList.remove('arrow-up');
+        footerArrow.classList.add('arrow-down');
         open = true;
 
     }
 
     function closeMenu(){
-        footerArrow.classList.remove('arrow-down');                                                
-        footerArrow.classList.add('arrow-up'); 
-        footer.style.height = '36px'; 
+        footerArrow.classList.remove('arrow-down');
+        footerArrow.classList.add('arrow-up');
+        footer.style.height = '36px';
         open = false;
     }
-    
+
 };
