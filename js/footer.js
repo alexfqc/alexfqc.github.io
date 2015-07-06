ed2c8d0f88a3bd75c714eadd68d93dcc37aa66ad
Alessander França <alessander70@gmail.com>
2015-07-06 19:37:09 -0300

    footer fixed

--- js/footer.js
+++ js/footer.js
@@ -1,78 +1,79 @@
-window.onload = function() {
+var footer = document.getElementById('footer');
+var containerPage = document.getElementById('container-page');
+var footerArrow = document.getElementById('footer-arrow');
+var footerArrow_size = footerArrow.clientHeight;
+var footerC = document.getElementById('footer-container');
+var footer_size = footer.clientHeight;
+var footerC_size = footerC.clientHeight + 5;
+var githubLogo = document.getElementById('github-logo');
+var stackLogo = document.getElementById('stack-logo');
+var linkedinLogo = document.getElementById('linkedin-logo');
+var open = false;
+var window_height = window.innerHeight;
 
-  var footer = document.getElementById('footer');
-  var containerPage = document.getElementById('container-page');
-  var footerArrow = document.getElementById('footer-arrow');
-  var footerArrow_size = footerArrow.clientHeight;
-  var footerC = document.getElementById('footer-container');
-  var footer_size = footer.clientHeight;
-  var footerC_size = footerC.clientHeight + 5;
-  var githubLogo = document.getElementById('github-logo');
-  var stackLogo = document.getElementById('stack-logo');
-  var linkedinLogo = document.getElementById('linkedin-logo');
-  var open = false;
-  var window_height = window.innerHeight;
+footer.onclick = function() {
+  if (window.innerWidth < 768) {
+    if (open === false) {
 
-  footer.onclick = function() {
-    if (window.innerWidth < 768) {
-      if (open === false) {
-
-        if ( /*window.innerHeight < 550 &&*/ window.innerWidth >= 320 && window.innerWidth <= 767) {
-          githubLogo.style.transform = 'translateX(-90px)';
-          stackLogo.style.transform = 'translateY(-90px)';
-          linkedinLogo.style.transform = 'translateX(90px)';
-          githubLogo.style.opacity = '0';
-          stackLogo.style.opacity = '0';
-          linkedinLogo.style.opacity = '0';
-        }
-        openMenu();
-      } else {
-
-
-        if ( /*window.innerHeight < 550 &&*/ window.innerWidth >= 320 && window.innerWidth <= 767) {
-          githubLogo.style.transform = 'translateX(0px)';
-          stackLogo.style.transform = 'translateY(0px)';
-          linkedinLogo.style.transform = 'translateX(0px)';
-          githubLogo.style.opacity = '1';
-          stackLogo.style.opacity = '1';
-          linkedinLogo.style.opacity = '1';
-        }
-        closeMenu();
-
+      if ( /*window.innerHeight < 550 &&*/ window.innerWidth >= 320 && window.innerWidth <= 767) {
+        githubLogo.style.transform = 'translateX(-90px)';
+        stackLogo.style.transform = 'translateY(-90px)';
+        linkedinLogo.style.transform = 'translateX(90px)';
+        githubLogo.style.opacity = '0';
+        stackLogo.style.opacity = '0';
+        linkedinLogo.style.opacity = '0';
       }
-    }
-
-  }
-
-  window.onresize = function() {
-
-    if (window.innerWidth >= 768) {
-      footer.style.height = 'auto';
+      openMenu();
     } else {
+
+
+      if ( /*window.innerHeight < 550 &&*/ window.innerWidth >= 320 && window.innerWidth <= 767) {
+        githubLogo.style.transform = 'translateX(0px)';
+        stackLogo.style.transform = 'translateY(0px)';
+        linkedinLogo.style.transform = 'translateX(0px)';
+        githubLogo.style.opacity = '1';
+        stackLogo.style.opacity = '1';
+        linkedinLogo.style.opacity = '1';
+      }
       closeMenu();
-    }
 
+    }
   }
 
-  function openMenu() {
-    if (window.innerWidth == 320) {
-      footer.style.height = '180px';
-    }
-    else{
-      footer.style.height = '200px';
-    }
+}
 
-    footerArrow.classList.remove('arrow-up');
-    footerArrow.classList.add('arrow-down');
-    open = true;
+window.onresize = function() {
 
+  if (window.innerWidth >= 768) {
+    footer.style.height = 'auto';
+  } else {
+    closeMenu();
+    githubLogo.style.transform = 'translateX(0px)';
+    stackLogo.style.transform = 'translateY(0px)';
+    linkedinLogo.style.transform = 'translateX(0px)';
+    githubLogo.style.opacity = '1';
+    stackLogo.style.opacity = '1';
+    linkedinLogo.style.opacity = '1';
   }
 
-  function closeMenu() {
-    footerArrow.classList.remove('arrow-down');
-    footerArrow.classList.add('arrow-up');
-    footer.style.height = '36px';
-    open = false;
+}
+
+function openMenu() {
+  if (window.innerWidth == 320) {
+    footer.style.height = '180px';
+  } else {
+    footer.style.height = '200px';
   }
 
-};
+  footerArrow.classList.remove('arrow-up');
+  footerArrow.classList.add('arrow-down');
+  open = true;
+
+}
+
+function closeMenu() {
+  footerArrow.classList.remove('arrow-down');
+  footerArrow.classList.add('arrow-up');
+  footer.style.height = '36px';
+  open = false;
+}
